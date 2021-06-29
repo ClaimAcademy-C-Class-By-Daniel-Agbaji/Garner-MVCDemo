5fd6b7f777f019ac4d238cb0e80f07969bcc3766
Callidus91 <trgarner91@gmail.com>
2021-06-29 18:20:38 -0500

    Add project files.

---
+++ Data/AudioInfo.cs
@@ -0,0 +1,19 @@
+﻿using AudioMVC.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioMVC.Data
+{
+    public class AudioInfo : DbContext
+    {
+        public AudioInfo(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Audio> tblAudioDevices { get; set; }
+    }
+}
---
+++ Migrations/20210629231305_initialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AudioMVC.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblAudioDevices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Amps = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dac = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Headphones = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Iem = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblAudioDevices", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblAudioDevices");
+        }
+    }
+}
